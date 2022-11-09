9a4f2199b32efd753a55c03b9754e02d86c4ed42
vmcustodio <van.custodio.vc@gmail.com>
2022-11-08 21:41:51 -0300

    first commit

---
+++ versao-inicial/frontend/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+	<div id="app" :class="{'hide-menu': !isMenuVisible}">
+		<Header title="Cod3r - Base de Conhecimento" :hideToggle="false"/>
+		<Menu/>
+		<Content/>
+		<Footer/>
+	</div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Header from "@/components/templatee/Header"
+import Menu from "@/components/templatee/Menu"
+import Content from "@/components/templatee/Content"
+import Footer from "@/components/templatee/Footer"
+
+export default {
+	name: "App",
+	components: { Header, Menu, Content, Footer },
+	computed: mapState(['isMenuVisible'])
+}
+</script>
+
+<style>
+* {
+	font-family: "Lato", sans-serif;
+}
+
+body {
+	margin: 0;
+}
+
+#app {
+	--webkit-font-smoothing: antialiased;
+	--moz-osx-font-smoothin: grayscale;
+
+	height: 100vh;
+	display: grid;
+	grid-template-rows: 60px 1fr 40px;
+	grid-template-columns: 300px 1fr;
+	grid-template-areas: "header header" "menu content" "menu footer"
+}
+
+#app.hide-menu {
+	grid-template-areas: "header header" "content content" "footer footer" ;
+}
+</style>
---
+++ versao-inicial/frontend/src/main.js
@@ -0,0 +1,12 @@
+import 'font-awesome/css/font-awesome.css'
+import Vue from 'vue'
+import store from './config/store'
+
+import App from './App'
+
+Vue.config.productionTip = false
+
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount('#app')
