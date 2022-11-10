67b42dc16d2461534d55b082aa4445bcdbe9ac57
vmcustodio <van.custodio.vc@gmail.com>
2022-11-09 23:29:28 -0300

    feat: add useradmin

--- versao-inicial/frontend/src/components/admin/UserAdmin.vue
+++ versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -1,12 +1,41 @@
 <template>
   <div class="user-admin">
-    <h1>Usuarios Componente</h1>
+    <b-table hover striped :item="users" :fields="fields"></b-table>
   </div>
 </template>
 
 <script>
+import { baseApiUrl } from '@/global'
+import axios from 'axios'
+
 export default {
-  name: 'UserAdmin'
+  name: 'UserAdmin',
+  data: function() {
+    return {
+      mode: 'save',
+      user: {},
+      users: [], //array que vai ser exibido na tabela
+      fields: [
+        { key: 'id', label: 'Código', sortable: true},
+        { key: 'name', label: 'Nome', sortable: true},
+        { key: 'email', label: 'Email', sortable: true},
+        { key: 'admin', label: 'Administrador', sortable: true,
+          formatter: value => value ? 'Sim' : 'Não' },
+        { key: 'action', label: 'Ações'}
+      ]
+    }
+  },
+  methods: {
+    loadUsers() {
+      const url = `${baseApiUrl}/users`
+      axios.get(url).then(res => {
+        this.users = res.data
+      })
+    }
+  },
+  mounted() {
+    this.loadUsers()
+  }
 }
 </script>
 
