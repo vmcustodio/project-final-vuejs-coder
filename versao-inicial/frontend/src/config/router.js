8309096a3b1d2be3463e74bf7d23c6764b6f3cd5
vmcustodio <van.custodio.vc@gmail.com>
2022-11-16 00:38:11 -0300

    feat: show article

--- versao-inicial/frontend/src/config/router.js
+++ versao-inicial/frontend/src/config/router.js
@@ -4,26 +4,28 @@ import VueRouter from 'vue-router'
 import Home from '@/components/home/Home'
 import AdminPages from '@/components/admin/AdminPages'
 import ArticlesByCategory from '@/components/article/ArticlesByCategory'
+import ArticleById from '@/components/article/ArticleById'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
   name: 'home',
   path: '/',
   component: Home
-  }, 
-  {
-    name: 'adminPages',
-    path: '/admin',
-    component: AdminPages
-  }, 
-  {
+}, {
+  name: 'adminPages',
+  path: '/admin',
+  component: AdminPages,
+  meta: { requiresAdmin: true }
+}, {
   name: 'articlesByCategory',
   path: '/categories/:id/articles',
   component: ArticlesByCategory
-  }
-]
+}, {
+  name: 'articleById',
+  path: '/articles/:id',
+  component: ArticleById
+}]
 
 export default new VueRouter({
   mode: 'history',
