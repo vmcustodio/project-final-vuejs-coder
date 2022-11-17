a6377f48586494ad8c7687663e0d2563e29cd428
vmcustodio <van.custodio.vc@gmail.com>
2022-11-16 23:16:31 -0300

    feat: add validAdmin

--- versao-inicial/frontend/src/config/router.js
+++ versao-inicial/frontend/src/config/router.js
@@ -6,6 +6,7 @@ import AdminPages from '@/components/admin/AdminPages'
 import ArticlesByCategory from '@/components/article/ArticlesByCategory'
 import ArticleById from '@/components/article/ArticleById'
 import Auth from '@/components/auth/Auth'
+import { userKey } from '@/global'
 
 Vue.use(VueRouter)
 
@@ -32,7 +33,20 @@ const routes = [{
   component: Auth
 }]
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: 'history',
   routes: routes
 })
+
+router.beforeEach((to, from, next) => {
+  const json = localStorage.getItem(userKey)
+
+  if(to.matched.some(record => record.meta.requiresAdmin)) {
+    const user = JSON.parse(json)
+    user && user.admin ? next() : next({ path: '/'})
+  } else {
+    next()
+  }
+})
+
+export default router
